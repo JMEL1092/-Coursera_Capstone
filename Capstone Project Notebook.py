baf007c3e3fecf8efa20602fe26e14174272d1cf
JMEL1092 <59812343+JMEL1092@users.noreply.github.com>
2020-01-12 21:25:28 -0600

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This notebook will be mainly use for my capstone project
+
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print (' Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
